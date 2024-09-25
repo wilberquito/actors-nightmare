d27fe19ad075a62e38e76ca7b02218296120abac
wilberquito <wbq.software@gmail.com>
2024-09-25 12:52:34 +0200

    ref: improve readibility

--- src/main/scala/PrimersNaive.scala
+++ src/main/scala/PrimersNaive.scala
@@ -1,14 +1,19 @@
+/*
+* The program counts how many primes there are in a given range of integers.
+*/
 
 object PrimersNaive extends App {
   val max = 10000000
+
   def isPrime(n: Int): Boolean = new java.math.BigInteger("" + n).isProbablePrime(20)
 
   println(s"Anem a comptar primers, de 2 a $max")
-  val temps = System.nanoTime
-  val answer = (2 to max).count(isPrime)
 
-  println(s"Hi ha $answer primers")
-  println("S'ha trigat: " + (System.nanoTime - temps) / 1e9d + " segons")
+  val t0 = System.nanoTime
+  val nPrimes = (2 to max).count(isPrime)
+
+  println(s"Hi ha $nPrimes primers")
+  println("S'ha trigat: " + (System.nanoTime - t0) / 1e9d + " segons")
 }
 
 
